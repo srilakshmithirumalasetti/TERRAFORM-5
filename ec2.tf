613a313a05395348a5fa6659369da42a56ad9d45
Pravalika-27 <112800726+Pravalika-27@users.noreply.github.com>
2025-01-07 19:13:44 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "my_instance" {
 }
 
 resource "aws_security_group" "demosg" {
-  name        = "newsg"
+  name        = "updated instance"
   description = "Security group for WordPress and MySQL"
 
   ingress {
